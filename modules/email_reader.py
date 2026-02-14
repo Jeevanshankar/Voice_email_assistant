8508c30857ae8e2d0e4eccf87e2489ddd99d9196
Jeevanshankar <jeevanshankarkompally@gmail.com>
2026-02-14 22:04:44 +0530

    Load Google OAuth credentials from env

--- modules/email_reader.py
+++ modules/email_reader.py
@@ -1,8 +1,10 @@
+import json
 import os, base64
 from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
+from google_auth_oauthlib.flow import Flow, InstalledAppFlow
 from google.oauth2.credentials import Credentials
 from google.auth.transport.requests import Request
+
 
 SCOPES = [
     "https://www.googleapis.com/auth/gmail.readonly",
@@ -18,8 +20,14 @@ def _get_service():
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
-            creds = flow.run_local_server(port=0)
+            client_config = json.loads(os.environ["GOOGLE_CREDENTIALS"])
+        flow = Flow.from_client_config(
+            client_config,
+            scopes=SCOPES,
+            redirect_uri=os.environ.get("OAUTH_REDIRECT_URI")
+)
+
+        creds = flow.run_local_server(port=0)
 
         with open("token.json", "w") as token:
             token.write(creds.to_json())
